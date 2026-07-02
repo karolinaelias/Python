601569a8e1a4a9cc85042cdbd6951902d02db164
karolinaelias <karoleliasbr@gmail.com>
2026-07-02 12:48:59 -0300

    Update Desafio 5.py

--- pacote-dowload/Aulas de Python/Desafios/Desafio 5.py
+++ pacote-dowload/Aulas de Python/Desafios/Desafio 5.py
@@ -1,5 +1,62 @@
 lista = [ ]
 
+def adicionar_nome():
+    novo_nome = input("Digite o nome a ser cadastrado:")
+    if novo_nome == "":
+        print("Nome inválido.")
+    else:
+        lista.append(novo_nome)
+        print("Nome cadastrado com sucesso!")
+
+def exibir_nomes():
+    if len(lista) == 0:
+        print("Nenhum nome cadastrado ainda.")
+    else:
+        print("==== Nomes Cadastrados ====")
+        for nome in lista:
+            print(nome)
+            print("-" * 10)
+
+def buscar_nome():
+    busca_nome = input("Digite o nome a ser procurado:")
+    if busca_nome in lista:
+        print("O nome está cadastrado na lista.")
+    else:
+        print("O nome não está cadastrado na lista.")
+
+def editar_nome():
+    nome_atual = input("Digite o nome a ser editado:")
+    if nome_atual in lista:
+        posicao = lista.index(nome_atual)
+        novo_nome = input("Digite o novo nome:")
+        lista[posicao] = novo_nome
+        print("Nome alterado com sucesso!")
+    else:
+        print("Nome não encontrado.")
+
+def remover_nome():    
+    nome = input("Digite o nome a ser removido:")
+    if nome in lista:
+        lista.remove(nome)
+        print("Nome removido com sucesso!")
+    else:
+        print("O nome não está cadastrado na lista")
+
+def encerrar_programa():
+    print("Encerrando programa ...")
+    return False
+
+resultado = encerrar_programa
+
+opcoes = {
+    "1": adicionar_nome,
+    "2": exibir_nomes,
+    "3": buscar_nome,
+    "4": editar_nome,
+    "5": remover_nome,
+    "6": encerrar_programa
+    }
+
 while True:
     print("1 - Adicionar nome na lista")
     print("2 - Exibir nomes cadastrados")
@@ -10,45 +67,9 @@ while True:
     
     opcao_escolhida = input("Escolha uma opção:")
 
-    if opcao_escolhida == "1":
-        novo_nome = input("Digite o nome a ser cadastrado:")
-        if novo_nome == "":
-            print("Nome inválido.")
-        else:
-            lista.append(novo_nome)
-            print("Nome cadastrado com sucesso!")
-    elif opcao_escolhida == "2":
-        if len(lista) == 0:
-            print("Nenhum nome cadastrado ainda.")
-        else:
-            print("==== Nomes Cadastrados ====")
-            for nome in lista:
-                print(nome)
-                print("-" * 10)
-    elif opcao_escolhida == "3":
-        busca_nome = input("Digite o nome a ser procurado:")
-        if busca_nome in lista:
-            print("O nome está cadastrado na lista.")
-        else:
-            print("O nome não está cadastrado na lista.")
-    elif opcao_escolhida == "4":
-        nome_atual = input("Digite o nome a ser editado:")
-        if nome_atual in lista:
-            posicao = lista.index(nome_atual)
-            novo_nome = input("Digite o novo nome:")
-            lista[posicao] = novo_nome
-            print("Nome alterado com sucesso!")
-        else:
-            print("Nome não encontrado.")
-    elif opcao_escolhida == "5":    
-        nome = input("Digite o nome a ser removido:")
-        if nome in lista:
-            lista.remove(nome)
-            print("Nome removido com sucesso!")
-        else:
-            print("O nome não está cadastrado na lista")
-    elif opcao_escolhida == "6": 
-        print("Encerrando programa...")
-        break
-    else: 
+    if not opcao_escolhida in opcoes:
         print("Opção inválida. Tente novamente.")
+    else: 
+       resultado = opcoes[opcao_escolhida]()
+       if resultado == False:
+           break
